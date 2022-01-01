2c1cdacb89dbb3dc221efd32fb0ad97710fe4b91
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2022-01-01 15:54:44 -0300

    Home styling

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+
+    return (
+        <button className="btn btn-primary bt-icon">
+            <h6>INICIE AGORA A SUA BUSCA</h6>
+        </button>
+    );
+}
+
+export default ButtonIcon;
