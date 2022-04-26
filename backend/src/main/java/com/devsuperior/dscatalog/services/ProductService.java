a5b4975e4c3540a68baa35fa1780639ff9a019d2
CleristonMP <cleriston.melo.pereira@gmail.com>
2022-04-26 11:06:24 -0300

    Fix backend

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -43,7 +43,7 @@ public class ProductService {
 		List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
 		Page<Product> page = repository.find(pageable, categories, name);
 		repository.findProductsWithCategories(page.getContent());
-		return page.map(x -> new ProductDTO(x));
+		return page.map(x -> new ProductDTO(x, x.getCategories()));
 	}
 
 	@Transactional(readOnly = true)
