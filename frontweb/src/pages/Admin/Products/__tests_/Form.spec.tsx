2bfa8ee6d8d4055cfcde12d038849682bb929787
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2022-05-24 12:02:44 -0300

    test update should show toast and redirect when submit form correctly

--- frontweb/src/pages/Admin/Products/__tests_/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests_/Form.spec.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable testing-library/no-wait-for-multiple-assertions */
 import { render, screen, waitFor } from '@testing-library/react';
 import Form from '../Form';
 import { Router, useParams } from 'react-router-dom';
 import history from 'util/history';
 import userEvent from '@testing-library/user-event';
-import { server } from './fixtures';
+import { productResponse, server } from './fixtures';
 import selectEvent from 'react-select-event';
 import { ToastContainer } from 'react-toastify';
 
@@ -12,95 +13,138 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useParams: jest.fn(),
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
 }));
 
 describe('Product for create tests', () => {
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({ productId: 'create' });
+  });
+
+  test('should show toast and redirect when submit form correctly', async () => {
+    render(
+      <Router history={history}>
+        <ToastContainer />
+        <Form />
+      </Router>
+    );
+
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+    const categoriesInput = screen.getByLabelText('Categorias');
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+    await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+    userEvent.type(nameInput, 'Computador');
+    userEvent.type(priceInput, '5000.12');
+    userEvent.type(imgUrlInput, 'https://example.com');
+    userEvent.type(descriptionInput, 'Computador muito bom');
+
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const toastElement = screen.getByText('Produto cadastrado com sucesso');
+      expect(toastElement).toBeInTheDocument();
+    });
+
+    expect(history.location.pathname).toEqual('/admin/products');
+  });
+
+  test('should show 5 validation messages when just click submit', async () => {
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const messages = screen.getAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(5);
+    });
+  });
+
+  test('should clear validation messages when filling out form correctly', async () => {
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const messages = screen.getAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(5);
+    });
+
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+    const categoriesInput = screen.getByLabelText('Categorias');
+
+    await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+    userEvent.type(nameInput, 'Computador');
+    userEvent.type(priceInput, '5000.12');
+    userEvent.type(imgUrlInput, 'https://example.com');
+    userEvent.type(descriptionInput, 'Computador muito bom');
+
+    await waitFor(() => {
+      const messages = screen.queryAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(0);
+    });
+  });
+
+  describe('Product for update tests', () => {
     beforeEach(() => {
-        (useParams as jest.Mock).mockReturnValue({ productId: 'create' });
+      (useParams as jest.Mock).mockReturnValue({ productId: '2' });
     });
 
     test('should show toast and redirect when submit form correctly', async () => {
-        render(
-            <Router history={history}>
-                <ToastContainer />
-                <Form />
-            </Router>
-        );
+      render(
+        <Router history={history}>
+          <ToastContainer />
+          <Form />
+        </Router>
+      );
 
+      await waitFor(() => {
         const nameInput = screen.getByTestId('name');
         const priceInput = screen.getByTestId('price');
         const imgUrlInput = screen.getByTestId('imgUrl');
         const descriptionInput = screen.getByTestId('description');
-        const categoriesInput = screen.getByLabelText('Categorias');
 
-        const submitButton = screen.getByRole('button', { name: /salvar/i });
+        const formElement = screen.getByTestId("form");
 
-        await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
-        userEvent.type(nameInput, 'Computador');
-        userEvent.type(priceInput, '5000.12');
-        userEvent.type(imgUrlInput, 'https://example.com');
-        userEvent.type(descriptionInput, 'Computador muito bom');
+        expect(nameInput).toHaveValue(productResponse.name);
+        expect(priceInput).toHaveValue(String(productResponse.price));
+        expect(imgUrlInput).toHaveValue(productResponse.imgUrl);
+        expect(descriptionInput).toHaveValue(productResponse.description);
 
-        userEvent.click(submitButton);
+        const ids = productResponse.categories.map(x => String(x.id));
+        expect(formElement).toHaveFormValues({categories: ids});
+      });
 
-        await waitFor(() => {
-            const toastElement = screen.getByText('Produto cadastrado com sucesso');
-            expect(toastElement).toBeInTheDocument();
-        });
+      const submitButton = screen.getByRole('button', { name: /salvar/i });
 
-        expect(history.location.pathname).toEqual('/admin/products');
+      userEvent.click(submitButton);
+
+      await waitFor(() => {
+        const toastElement = screen.getByText('Produto cadastrado com sucesso');
+        expect(toastElement).toBeInTheDocument();
+      });
+
+      expect(history.location.pathname).toEqual('/admin/products');
     });
-
-    test('should show 5 validation messages when just click submit', async () => {
-        render(
-            <Router history={history}>
-                <Form />
-            </Router>
-        );
-
-        const submitButton = screen.getByRole('button', { name: /salvar/i });
-
-        userEvent.click(submitButton);
-
-        await waitFor(() => {
-            const messages = screen.getAllByText('Campo obrigatório');
-            expect(messages).toHaveLength(5);
-        });
-    });
-
-    test('should clear validation messages when filling out form correctly', async () => {
-        render(
-            <Router history={history}>
-                <Form />
-            </Router>
-        );
-
-        const submitButton = screen.getByRole('button', { name: /salvar/i });
-
-        userEvent.click(submitButton);
-
-        await waitFor(() => {
-            const messages = screen.getAllByText('Campo obrigatório');
-            expect(messages).toHaveLength(5);
-        });
-
-        const nameInput = screen.getByTestId('name');
-        const priceInput = screen.getByTestId('price');
-        const imgUrlInput = screen.getByTestId('imgUrl');
-        const descriptionInput = screen.getByTestId('description');
-        const categoriesInput = screen.getByLabelText('Categorias');
-
-        await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
-        userEvent.type(nameInput, 'Computador');
-        userEvent.type(priceInput, '5000.12');
-        userEvent.type(imgUrlInput, 'https://example.com');
-        userEvent.type(descriptionInput, 'Computador muito bom');
-
-        await waitFor(() => {
-            const messages = screen.queryAllByText('Campo obrigatório');
-            expect(messages).toHaveLength(0);
-        });
-    });
+  });
 });
