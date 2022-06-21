e2b6328d6df4439a0e65a05854c965edb01e28d0
CleristonMP <cleriston.melo.pereira@gmail.com>
2022-06-21 15:25:40 -0300

    refactor: Routes for Users page

--- frontweb/src/pages/Admin/Users/index.tsx
+++ frontweb/src/pages/Admin/Users/index.tsx
@@ -1,86 +1,16 @@
-import { AxiosRequestConfig } from 'axios';
-import Pagination from 'components/Pagination';
-import { useCallback, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { User } from 'types/user';
-import { SpringPage } from 'types/vendor/spring';
-import { requestBackend } from 'util/requests';
-import UserCrudCard from './UserCrudCard';
-import UserFilter, { UserFilterData } from './UserFilter';
-
-import './styles.css';
-
-type ControlComponentsData = {
-  activePage: number;
-  filterData: UserFilterData;
-};
+import { Route, Switch } from 'react-router-dom';
+import List from './List';
 
 const Users = () => {
-  const [page, setPage] = useState<SpringPage<User>>();
-
-  const [controlComponentsData, setControlComponentsData] =
-    useState<ControlComponentsData>({
-      activePage: 0,
-      filterData: { name: '', sort: 'desc' },
-    });
-
-  const handlePageChange = (pageNumber: number) => {
-    setControlComponentsData({
-      activePage: pageNumber,
-      filterData: controlComponentsData.filterData,
-    });
-  };
-
-  const handleSubmitFilter = (data: UserFilterData) => {
-    setControlComponentsData({ activePage: 0, filterData: data });
-  };
-
-  const getUsers = useCallback(() => {
-    const config: AxiosRequestConfig = {
-      method: 'GET',
-      url: '/users',
-      withCredentials: true,
-      params: {
-        page: controlComponentsData.activePage,
-        size: 3,
-        name: controlComponentsData.filterData.name,
-        sort: `id,${controlComponentsData.filterData.sort}`,
-      },
-    };
-
-    requestBackend(config).then((response) => {
-      setPage(response.data);
-    });
-  }, [controlComponentsData]);
-
-  useEffect(() => {
-    getUsers();
-  }, [getUsers]);
-
   return (
-    <div className="user-crud-container">
-      <div className="user-crud-bar-container">
-        <Link to="/admin/users/create">
-          <button className="btn btn-primary text-white btn-crud-add">
-            ADICIONAR
-          </button>
-        </Link>
-        <UserFilter onSubmitFilter={handleSubmitFilter} />
-      </div>
-      <div>
-        {page?.content.map((item) => (
-          <div key={item.id}>
-            <UserCrudCard user={item} onDelete={getUsers} />
-          </div>
-        ))}
-      </div>
-      <Pagination
-        forcePage={page?.number}
-        pageCount={page ? page.totalPages : 0}
-        range={3}
-        onChange={handlePageChange}
-      />
-    </div>
+    <Switch>
+      <Route path="/admin/users" exact>
+        <List />
+      </Route>
+      <Route path="/admin/users/:userId">
+        <h1>Form</h1>
+      </Route>
+    </Switch>
   );
 };
 
