ace7171e1879ce80eed1766a2afb42ba89552a82
CleristonMP <cleriston.melo.pereira@gmail.com>
2022-07-18 21:02:12 -0300

    feat: Signup completed

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -5,7 +5,7 @@ import { requestBackendLogin } from 'util/requests';
 import { useContext, useState } from 'react';
 import { AuthContext } from 'AuthContext';
 import { saveAuthData } from 'util/storage';
-import { getTokenData } from "util/token";
+import { getTokenData } from 'util/token';
 
 import './styles.css';
 
@@ -16,7 +16,7 @@ type CredentialsDTO = {
 
 type LocationState = {
   from: string;
-}
+};
 
 const Login = () => {
   const location = useLocation<LocationState>();
@@ -27,7 +27,11 @@ const Login = () => {
 
   const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit, formState: { errors } } = useForm<CredentialsDTO>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CredentialsDTO>();
 
   const history = useHistory();
 
@@ -64,27 +68,35 @@ const Login = () => {
               required: 'Campo obrigatório',
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'Email inválido'
-              }
+                message: 'Email inválido',
+              },
             })}
             type="text"
-            className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
+            className={`form-control base-input ${
+              errors.username ? 'is-invalid' : ''
+            }`}
             placeholder="Email"
             name="username"
           />
-          <div className="invalid-feedback d-block">{errors.username?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-2">
           <input
             {...register('password', {
-              required: 'Campo obrigatório'
+              required: 'Campo obrigatório',
             })}
             type="password"
-            className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
+            className={`form-control base-input ${
+              errors.password ? 'is-invalid' : ''
+            }`}
             placeholder="Password"
             name="password"
           />
-          <div className="invalid-feedback d-block">{errors.password?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
@@ -94,7 +106,7 @@ const Login = () => {
         </div>
         <div className="signup-container">
           <span className="not-registered">Não tem Cadastro?</span>
-          <Link to="/admin/auth/register" className="login-link-register">
+          <Link to="/admin/auth/signup" className="login-link-register">
             CADASTRAR
           </Link>
         </div>
